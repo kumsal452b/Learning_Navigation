84e8141bbea240041f0cf7d33a673b7a65d476dc
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-09-03 20:56:04 +0300

    Create Drawe system

--- app/src/main/java/com/example/learning_navigation/MainActivity.java
+++ app/src/main/java/com/example/learning_navigation/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.learning_navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    Toole
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+
     }
 }

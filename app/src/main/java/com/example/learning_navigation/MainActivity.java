2d0f78ff6756ffd0ad9a8b300df3cbd40084b9cd
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-09-03 21:28:47 +0300

    End of day

--- app/src/main/java/com/example/learning_navigation/MainActivity.java
+++ app/src/main/java/com/example/learning_navigation/MainActivity.java
@@ -1,21 +1,50 @@
 package com.example.learning_navigation;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
-    Toole
+
+    ActionBarDrawerToggle barDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar=findViewById(R.id.toolbar);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+        barDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(barDrawerToggle);
+        barDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (barDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
 
     }
 }

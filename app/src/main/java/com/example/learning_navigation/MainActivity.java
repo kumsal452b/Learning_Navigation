50242a5dcc065746a9c6092256620aa4191656a0
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-09-04 20:49:38 +0300

    Nothing

--- app/src/main/java/com/example/learning_navigation/MainActivity.java
+++ app/src/main/java/com/example/learning_navigation/MainActivity.java
@@ -11,6 +11,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
+import static com.example.learning_navigation.R.id.fragment_conteiner;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +22,37 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
 
     ActionBarDrawerToggle barDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar=findViewById(R.id.toolbar);
-        drawerLayout=findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
         setSupportActionBar(toolbar);
-        barDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        barDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(barDrawerToggle);
         barDrawerToggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_bar);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_message:
+                        getSupportFragmentManager().beginTransaction().replace(fragment_conteiner,new message()).commit();
+                        break;
+
+
+                }
+                return false;
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (barDrawerToggle.onOptionsItemSelected(item)){
+        if (barDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -40,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
 
